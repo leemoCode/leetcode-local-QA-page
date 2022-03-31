1b3a4cd2345dbd1e9c136e247618010df3537d88
leemoCode <L777888@88.com>
2022-03-31 22:48:37 +0800

    feat: Complete some inspection settings

---
+++ src/components/Template.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ msg }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Template",
+  components: {},
+  setup() {
+    const msg = "Component: Template";
+    return {
+      msg,
+    };
+  },
+});
+</script>
